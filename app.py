08db00d8cde548511d313b564980eef6c36dfa85
jramosmidagri-source <jramos.midagri@gmail.com>
2025-10-31 16:45:21 -0500

    Update app.py nuevo tamaño de letra

--- app.py
+++ app.py
@@ -152,9 +152,9 @@ def generar_reporte():
             except Exception:
                 return ImageFont.load_default()
 
-        font_title = safe_font(font_bold_path, 20)
-        font_body = safe_font(font_regular_path, 18)
-        font_bold = safe_font(font_bold_path, 20)
+        font_title = safe_font(font_bold_path, 19)
+        font_body = safe_font(font_regular_path, 15)
+        font_bold = safe_font(font_bold_path, 18)
 
         # === 5. Configuración del texto ===
         x0, y0 = 430, 350
