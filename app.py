aaf8b9b38b0850c283f53a38c5a4635462b7d31f
jramosmidagri-source <jramos.midagri@gmail.com>
2025-11-03 10:59:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -175,8 +175,8 @@ def generar_reporte():
                 return ImageFont.load_default()
 
         font_title = safe_font(font_bold_path, 23)
-        font_body = safe_font(font_regular_path, 24)
-        font_bold = safe_font(font_bold_path, 24)
+        font_body = safe_font(font_regular_path, 22)
+        font_bold = safe_font(font_bold_path, 22)
 
         # === 5. Configuración del texto ===
         x0, y0 = 280, 250
@@ -239,7 +239,7 @@ def generar_reporte():
         y_text = y0 + 10
         for linea in titulo_lineas:
             linea_width = get_text_width(draw, linea, font_title)
-            linea_x = x0 + (max_width - linea_width) / 2 - 20
+            linea_x = x0 + (max_width - linea_width) / 2 - 10
             draw.text((linea_x, y_text), linea, font=font_title, fill="black")
             y_text += font_title.getbbox(linea)[3] + 4
 
